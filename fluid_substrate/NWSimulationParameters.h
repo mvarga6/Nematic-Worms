48c4b799518cfcf51316ebf395ec79bde2121f7c
mvarga6 <mvarga6@kent.edu>
2016-03-16 16:56:50 -0400

    Simulation parameters for equilibrium and cross-linking stages.  Still to release box contraint before adding activity.

--- fluid_substrate/NWSimulationParameters.h
+++ fluid_substrate/NWSimulationParameters.h
@@ -13,17 +13,23 @@
 ----------------------------------------------------------------------------*/
 typedef struct {
 
-	//.. # of time steps
+	//.. # of time steps once cross-linked
 	int _NSTEPS;
+
+	//.. number of inner time steps for fast potential calculations
+	int _NSTEPS_INNER;
+
+	//.. number of steps of equilization before cross-linking
+	int _NSTEPS_EQUIL;
+
+	//.. number of steps allowed for cross-linking
+	int _NSTEPS_XLINK;
 
 	//.. rate of printing frames, and max frames per output file
 	int _FRAMERATE, _FRAMESPERFILE;
 
 	//.. long time integration constant
 	float _DT;
-
-	//.. number of inner time steps for fast potential calculations
-	int _NSTEPS_INNER;
 
 	//..system physical size
 	float _XBOX, _YBOX;
@@ -47,14 +53,16 @@ cudaError_t ParametersToDevice(SimulationParameters &params){
 --------------------------------------------------------------------------*/
 namespace DEFAULT {
 	namespace SIM {
-		static const int NSTEPS = 100000;
+		static const int NSTEPS = 1000000;
 		static const int NSTEPS_INNER = 10;
-		static const int FRAMERATE = 1000;
-		static const int FRAMESPERFILE = 100;
+		static const int NSTEPS_EQUIL = 100000;
+		static const int NSTEPS_XLINK = 50000;
+		static const int FRAMERATE = 5000;
+		static const int FRAMESPERFILE = 40;
 		static const float DT = 0.01f;
-		static const float XBOX = 100.0f;
-		static const float YBOX = 100.0f;
-		static const std::string FILENAME = "output.xyz";
+		static const float XBOX = 2000.0f;
+		static const float YBOX = 2000.0f;
+		static const std::string FILENAME = "xlinker_simulation.xyz";
 	}
 }
 //--------------------------------------------------------------------------
@@ -66,44 +74,46 @@ void GrabParameters(SimulationParameters * parameters, int argc, char *argv[], s
 		std::string val;
 		if (arg == "-dt"){
 			if (i + 1 < argc){
-				//std::string val = argv[1 + i++];
 				parameters->_DT = std::strtof(argv[1 + i++], NULL);
 			}
 		}
 		else if (arg == "-xbox"){
 			if (i + 1 < argc){
-				//val = std::string(argv[1 + i++]);
 				parameters->_XBOX = (int)std::strtof(argv[1 + i++], NULL);
 			}
 		}
 		else if (arg == "-ybox"){
 			if (i + 1 < argc){
-				//val = std::string(argv[1 + i++]);
 				parameters->_YBOX = (int)std::strtof(argv[1 + i++], NULL);
 			}
 		}
 		else if (arg == "-nsteps"){
 			if (i + 1 < argc){
-				//val = std::string(argv[1 + i++]);
 				parameters->_NSTEPS = (int)std::strtof(argv[1 + i++], NULL);
 			}
 		}
-		// Change
 		else if (arg == "-nsteps-inner"){
 			if (i + 1 < argc){
-				//val = std::string(argv[1 + i++]);
 				parameters->_NSTEPS_INNER = (int)std::strtof(argv[1 + i++], NULL);
+			}
+		}
+		else if (arg == "-nsteps-equil"){
+			if (i + 1 < argc){
+				parameters->_NSTEPS_EQUIL = (int)std::strtof(argv[1 + i++], NULL);
+			}
+		}
+		else if (arg == "-nsteps-xlink"){
+			if (i + 1 < argc){
+				parameters->_NSTEPS_XLINK = (int)std::strtof(argv[1 + i++], NULL);
 			}
 		}
 		else if (arg == "-framerate"){
 			if (i + 1 < argc){
-				//val = std::string(argv[1 + i++]);
 				parameters->_FRAMERATE = (int)std::strtof(argv[1 + i++], NULL);
 			}
 		}
 		else if (arg == "-framesperfile"){
 			if (i + 1 < argc){
-				//val = std::string(argv[1 + i++]);
 				parameters->_FRAMESPERFILE = (int)std::strtof(argv[1 + i++], NULL);
 			}
 		}
@@ -123,6 +133,7 @@ void Init(SimulationParameters * parameters, int argc, char *argv[], std::string
 	parameters->_YBOX = DEFAULT::SIM::YBOX;
 	parameters->_NSTEPS = DEFAULT::SIM::NSTEPS;
 	parameters->_NSTEPS_INNER = DEFAULT::SIM::NSTEPS_INNER;
+	parameters->_NSTEPS_EQUIL = DEFAULT::SIM::NSTEPS_EQUIL;
 	parameters->_FRAMERATE = DEFAULT::SIM::FRAMERATE;
 	parameters->_FRAMESPERFILE = DEFAULT::SIM::FRAMESPERFILE;
 	outfile = DEFAULT::SIM::FILENAME;
